25724cda343faa028ee6cd45587e90ea4a90aa80
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-06-12 20:15:52 -0400

    add ssl config for production env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -13,10 +13,11 @@ module.exports = ({ env }) => ({
         database: config.database,
         username: config.user,
         password: config.password,
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        },
       },
-      options: {
-        ssl: false,
-      },
+      options: {},
     },
   },
 });
